1e8f2b2038ebef8ed8a16ad2a9447a985a42809c
Tobias Hegemann <tobias.hegemann@googlemail.com>
2021-03-29 02:18:42 +0200

    current

--- dist/src/store/IStore.js
+++
@@ -1,1 +0,0 @@
-//# sourceMappingURL=IStore.js.map
--- dist/types/model/VolumeProperties.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.DefaultVolumeProperties = void 0;
-const DefaultVolumeProperties = {
-    volume: 1,
-    muted: false,
-};
-exports.DefaultVolumeProperties = DefaultVolumeProperties;
-//# sourceMappingURL=VolumeProperties.js.map
