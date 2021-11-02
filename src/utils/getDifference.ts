4b3ba0895a644a7fb9591d9c74ee205730852cc1
delude88 <tobias.hegemann@googlemail.com>
2021-11-02 15:41:38 +0100

    feature: added diff for updates

--- src/utils/getDifference.ts
+++ src/utils/getDifference.ts
@@ -1,5 +1,15 @@
 /* eslint-disable @typescript-eslint/no-explicit-any,@typescript-eslint/no-unsafe-argument,@typescript-eslint/no-unsafe-member-access,@typescript-eslint/no-unsafe-assignment */
-const getDifference = (a: any, b: any): any =>
-    Object.entries(a).reduce((ac, [k, v]) => (b[k] && b[k] !== v ? ((ac[k] = b[k]), ac) : ac), {})
+
+import isEqual from 'lodash/isEqual'
+
+const getDifference = (a: Record<string, any>, b: Record<string, any>): Record<string, any> => {
+    return Object.entries(a).reduce<Record<string, any>>((ac, [k, v]) => {
+        if (b[k] && !isEqual(b[k], v)) {
+            ac[k] = b[k]
+            return ac
+        }
+        return ac
+    }, {})
+}
 
 export { getDifference }
